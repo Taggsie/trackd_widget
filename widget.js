1a66b588915f4cd05aef978f384df612aad7f25c
Izerrion <71769024+Izerrion@users.noreply.github.com>
2024-01-08 15:50:31 +0300

    Update widget.js with valid JS syntax

--- widget.js
+++ widget.js
@@ -1,9 +1,10 @@
-<script>
-        var $zoho = $zoho || {};
-        $zoho.salesiq = $zoho.salesiq || {
-            values : {},
-            widgetcode: 'siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94'
-        };
-    </script>
-    <script src="https://salesiq.zohopublic.com/widget?widgetcode=siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94">
-</script>
+var $zoho = $zoho || {};
+$zoho.salesiq = $zoho.salesiq || {
+    values: {},
+    widgetcode: 'siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94'
+};
+
+// Include the SalesIQ widget script
+var script = document.createElement('script');
+script.src = 'https://salesiq.zohopublic.com/widget?widgetcode=siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94';
+document.head.appendChild(script);
