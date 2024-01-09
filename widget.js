97d55b946e2cdbfd6f5aa91eac22396f9b50d6e8
Taggsie <151899372+Taggsie@users.noreply.github.com>
2024-01-09 10:39:37 +0800

    Update widget.js
    Amended widget code to use alternative widget.

--- widget.js
+++ widget.js
@@ -1,10 +1,10 @@
 var $zoho = $zoho || {};
 $zoho.salesiq = $zoho.salesiq || {
     values: {},
-    widgetcode: 'siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94'
+    widgetcode: 'siqecba2aa9e3f9d84f8bae3909a8bd0e08d1b086d6dc02445a7a8b478985e59181'
 };
 
 // Include the SalesIQ widget script
 var script = document.createElement('script');
-script.src = 'https://salesiq.zohopublic.com/widget?widgetcode=siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94';
+script.src = 'https://salesiq.zohopublic.com/widget?widgetcode=siqecba2aa9e3f9d84f8bae3909a8bd0e08d1b086d6dc02445a7a8b478985e59181';
 document.head.appendChild(script);
