fc3b8f522b50035ce9f17b2fb2c0eecd5d44d471
Taggsie <151899372+Taggsie@users.noreply.github.com>
2026-05-13 13:35:46 +0800

    Update widget.js
    Updated to new version of sales IQ code

--- widget.js
+++ widget.js
@@ -6,5 +6,5 @@ $zoho.salesiq = $zoho.salesiq || {
 
 // Include the SalesIQ widget script
 var script = document.createElement('script');
-script.src = 'https://salesiq.zohopublic.com/widget?widgetcode=siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94';
+script.src = 'https://salesiq.zohopublic.com/widget?wc=siqecba2aa9e3f9d84f8bae3909a8bd0e08b4a5c158472fb2bb3c8a644eac7fde94';
 document.head.appendChild(script);
